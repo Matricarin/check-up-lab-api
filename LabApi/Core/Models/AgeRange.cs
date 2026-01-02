c0e4d0a817753082101eb51ee2777c0fbe30c990
Matricarin <matricarin@gmail.com>
2026-01-02 17:17:47 +0500

    value objects were added

---
+++ LabApi/Core/Models/AgeRange.cs
@@ -0,0 +1,29 @@
+﻿namespace LabApi.Core.Models;
+
+public class AgeRange : ValueObject
+{
+    public int From { get; }
+    public int To { get; }
+
+    public AgeRange(int from, int to)
+    {
+        if (to <= 0)
+        {
+            throw new ArgumentException();
+        }
+
+        if (to <= from)
+        {
+            throw new ArgumentException();
+        }
+
+        From = from;
+        To = to;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return From;
+        yield return To;
+    }
+}
---
+++ LabApi/Core/Models/MeasurementUnit.cs
@@ -0,0 +1,21 @@
+﻿namespace LabApi.Core.Models;
+
+public sealed class MeasurementUnit : ValueObject
+{
+    public string Code { get; }
+    public string DisplayName { get; }
+
+    public static MeasurementUnit Milliliters => new("ml", "Milliliters");
+
+    private MeasurementUnit(string code, string displayName)
+    {
+        Code = code;
+        DisplayName = displayName;
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Code;
+        yield return DisplayName;
+    }
+}
