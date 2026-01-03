1d68d7b20246f19c0bb3a5a4d52fd4b9ee935ee2
Matricarin <matricarin@gmail.com>
2026-01-03 15:45:17 +0500

    data was seeded

---
+++ LabApi/Migrations/20260103104033_models-were-changed.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelswerechanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MeasurementCode",
+                table: "NormalValue");
+
+            migrationBuilder.RenameColumn(
+                name: "MeasurementName",
+                table: "NormalValue",
+                newName: "MeasurementUnit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MeasurementUnit",
+                table: "NormalValue",
+                newName: "MeasurementName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MeasurementCode",
+                table: "NormalValue",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
