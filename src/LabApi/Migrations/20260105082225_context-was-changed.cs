bd8d6e74f871468a0747f14cd9dbb9c137fac77e
Matricarin <matricarin@gmail.com>
2026-01-05 13:34:25 +0500

    new migration was added

---
+++ src/LabApi/Migrations/20260105082225_context-was-changed.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class contextwaschanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MeasurementUnit",
+                table: "NormalValue",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MeasurementUnit",
+                table: "NormalValue",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
