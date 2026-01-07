a92376198f8d12b64b6d8a88a68ceeae6c5cff3d
Matricarin <matricarin@gmail.com>
2026-01-07 11:10:38 +0500

    new roles were added

---
+++ src/LabApi/Shared/ApiRoles.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+
+namespace LabApi.Shared;
+
+public static class ApiRoles
+{
+    public const string Admin = "admin";
+
+    [UsedImplicitly] public static IReadOnlyList<string> AllRoles = Array.AsReadOnly([Admin, Consumer]);
+
+    public const string Consumer = "Consumer";
+}
