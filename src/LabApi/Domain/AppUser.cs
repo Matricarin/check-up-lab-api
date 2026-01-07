f2b02d334e8b0692b3fc4ecf9bfb01407736d4d7
Matricarin <matricarin@gmail.com>
2026-01-07 11:17:39 +0500

    new property for app  user was added

--- src/LabApi/Domain/AppUser.cs
+++ src/LabApi/Domain/AppUser.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.Identity;
 
 namespace LabApi.Domain;
 
 public sealed class AppUser : IdentityUser
 {
-    
+    [StringLength(maximumLength: 50, 
+        ErrorMessage = "Display name should be from 1 to 50 symbols", 
+        MinimumLength = 1)]
+    public required string DisplayName { get; set; }
 }
