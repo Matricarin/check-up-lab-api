7b97c8ab9b35a550f7290f4db7cac1ed8c79800b
Matricarin <matricarin@gmail.com>
2026-01-08 21:17:55 +0500

    try update

--- src/LabApi/Extensions/DbContextServicesExtensions.cs
+++ src/LabApi/Extensions/DbContextServicesExtensions.cs
@@ -6,11 +6,12 @@ namespace LabApi.Extensions;
 
 public static class DbContextServicesExtensions
 {
-    public static IServiceCollection AddDatabaseContext(this IServiceCollection services, 
+    public static IServiceCollection AddDatabaseContext(this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddDbContext<AppDbContext>(context => 
-            context.UseNpgsql(configuration.GetConnectionString("Postgres")));
+        services.AddDbContext<AppDbContext>(context =>
+            context.UseNpgsql(configuration.GetConnectionString("Postgres") ??
+                              Environment.GetEnvironmentVariable("ConnectionString__Postgres")));
         return services;
     }
 }
