e47a548909d83c94623b533e0597bcd0637825e4
Matricarin <matricarin@gmail.com>
2026-01-08 21:32:39 +0500

    from config read

--- src/LabApi/Application/Services/JwtGenerationService.cs
+++ src/LabApi/Application/Services/JwtGenerationService.cs
@@ -8,10 +8,16 @@ namespace LabApi.Application.Services;
 public sealed class JwtGenerationService : IJwtGenerationService
 {
     private readonly string? _secretKey;
-
+    private readonly string? _issuer;
+    private readonly string? _audience;
+    private readonly int? _expirationMinutes;
+    
     public JwtGenerationService(IConfiguration configuration)
     {
         _secretKey = configuration["Auth:Secret"];
+        _issuer = configuration["Auth:Issuer"];
+        _audience = configuration["Auth:Audience"];
+        _expirationMinutes = int.Parse(configuration["Auth:TokenLifetimeMinutes"] ?? "30");
     }
 
     public string GenerateJwtToken(AppUser user, IList<string> roles)
