50f774347b86fb9f240b93c661480d365b660d7d
Matricarin <matricarin@gmail.com>
2026-01-12 20:34:00 +0500

    service draft

--- src/LabApi/Application/Services/AuthService.cs
+++ src/LabApi/Application/Services/AuthService.cs
@@ -4,7 +4,6 @@ using LabApi.Application.Dtos.Login;
 using LabApi.Application.Dtos.Register;
 using LabApi.Application.Interfaces;
 using LabApi.Domain;
-using LabApi.Infrastructure.Data;
 using LabApi.Shared;
 
 using Microsoft.AspNetCore.Identity;
@@ -13,30 +12,25 @@ namespace LabApi.Application.Services;
 
 public sealed class AuthService : IAuthService
 {
-    private readonly AppDbContext _db;
     private readonly IJwtGenerationService _jwtGenerationService;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly SignInManager<AppUser> _signInManager;
     private readonly UserManager<AppUser> _userManager;
 
     public AuthService(UserManager<AppUser> userManager,
         RoleManager<IdentityRole> roleManager,
-        AppDbContext db,
+        SignInManager<AppUser> signInManager,
         IJwtGenerationService jwtGenerationService)
     {
         _userManager = userManager;
         _roleManager = roleManager;
-        _db = db;
+        _signInManager = signInManager;
         _jwtGenerationService = jwtGenerationService;
     }
 
     public async Task<RegisterResponseDto?> CreateUser(RegisterRequestDto request)
     {
-        AppUser appUser = new() 
-        { 
-            DisplayName = request.DisplayName, 
-            UserName = request.Email,
-            Email = request.Email
-        };
+        AppUser appUser = new() { DisplayName = request.DisplayName, UserName = request.Email, Email = request.Email };
 
         IdentityResult result = await _userManager.CreateAsync(appUser, request.Password);
 
@@ -50,16 +44,43 @@ public sealed class AuthService : IAuthService
         await _userManager.AddClaimsAsync(appUser,
             ApiPermissions.DefaultPermissions.Select(p => new Claim(ApiPermissions.PermissionClaimType, p)));
 
-        string token = _jwtGenerationService.GenerateJwtToken(appUser, new List<string> { ApiRoles.Consumer },
-            new List<string> { ApiPermissions.ClinicalTestsRead });
+        string token = _jwtGenerationService.GenerateJwtToken(appUser, new List<string> { ApiRoles.DefaultRole },
+            ApiPermissions.DefaultPermissions.ToList());
 
-        RegisterResponseDto response = new() { AccessToken = token, ExpiresAt =  };
+        RegisterResponseDto
+            response = new() { AccessToken = token, ExpiresAt =  }; //  TODO: in appsettings i am written earlier
 
         return response;
     }
 
     public async Task<LoginResponseDto?> LoginUser(LoginRequestDto request)
     {
-        throw new NotImplementedException();
+        AppUser? user = await _userManager.FindByEmailAsync(request.Email);
+
+        if (user == null)
+        {
+            return null;
+        }
+
+        SignInResult result = await _signInManager.CheckPasswordSignInAsync(user, request.Password, true);
+
+        if (!result.Succeeded)
+        {
+            return null;
+        }
+
+        IList<string> role = await _userManager.GetRolesAsync(user);
+        IList<Claim> claims = await _userManager.GetClaimsAsync(user);
+
+        List<string> permissions = claims.Where(c => c.Type == ApiPermissions.PermissionClaimType)
+            .Select(c => c.Value)
+            .ToList();
+
+        string token = _jwtGenerationService.GenerateJwtToken(user, role, permissions);
+
+        LoginResponseDto
+            response = new() { AccessToken = token, ExpiresAt =  }; //  TODO: in appsettings i am written earlier
+
+        return response;
     }
 }
